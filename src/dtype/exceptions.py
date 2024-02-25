91c12df026053721a566a547e5045d6addd61302
Adrien Soursou <adrien.soursou@gmail.com>
2024-02-25 23:16:53 +0100

    Refactoring dtype

--- src/dtype/exceptions.py
+++ src/dtype/exceptions.py
@@ -1,17 +1,11 @@
 class InvalidShapeError(ValueError):
 	def __init__(self):
-		super().__init__("Invalid shape")
+		super().__init__("invalid matrix shape.")
 
 class NotSquareError(ValueError):
 	def __init__(self):
-		super().__init__("Matrix is not square")
+		super().__init__("matrix is not square.")
 
 def unsupported_op(op, *args):
-	"""Returns a TypeError for an unsupported operand.
-
-	Arguments:
-	op -- the unsupported operand
-	*args -- the arguments passed to the operand
-	"""
 	args = list(map(lambda x: f"'{type(x).__name__}'", args))
 	raise TypeError(f"unsupported operand type(s) for {op}: {' and '.join(args)}")
--- src/dtype/matrix.py
+++ src/dtype/matrix.py
@@ -3,7 +3,6 @@ from copy import deepcopy
 from src.dtype import Complex, InvalidShapeError, NotSquareError
 
 def is_close(x, y, abs_tol=1e-9):
-	"""Returns whether two numbers are close enough to be considered equal."""
 	if isinstance(x, Complex):
 		return abs(x - y) < abs_tol
 	return math.isclose(x, y, abs_tol=abs_tol)
@@ -16,59 +15,21 @@ class Matrix:
 
 	@staticmethod
 	def identity(n: int, dtype=float):
-		"""Returns an identity matrix.
-
-		Args:
-			n (int): The size of the matrix.
-			dtype (type, optional): The type of the matrix. Defaults to float.
-
-		Returns:
-			Matrix: An identity matrix.
-		"""
 		return Matrix([[dtype(i == j) for j in range(n)] for i in range(n)])
 
 	def __init__(self, values: list):
-		"""Initializes a matrix.
-
-		Args:
-			values (list): A list of values.
-
-		Raises:
-			InvalidShapeError: If the matrix is not rectangular.
-			TypeError: If values is not a list of dtype.
-		"""
-		if values is None:
-			values = []
-		if (any(len(x) == 0 for x in values) or
-			any(len(x) != len(values[0]) for x in values)):
+		if any(len(x) != len(values[0]) for x in values):
 			raise InvalidShapeError
-		if len(values):
-			if (any(not isinstance(x, list) for x in values) or
-				any(any(not isinstance(y, type(values[0][0])) for y in x) for x in values)):
-				raise TypeError(f"values must be a 2d list of {type(values[0][0])}")
-			self.shape = (len(values), len(values[0]))
-		else:
-			self.shape = (0, 0)
+		if any(any(not isinstance(y, type(values[0][0])) for y in x) for x in values):
+			raise SyntaxError("all elements in the matrix must be of the same type.")
+		self.shape = (len(values), len(values[0]))
 		self.values = values
 
 	def _assert_same_shape(self, other):
-		"""Asserts that the matrices have the same shape.
-
-		Args:
-			other (Matrix): The other matrix.
-
-		Raises:
-			InvalidShapeError: If the matrices do not have the same shape.
-		"""
 		if self.shape != other.shape:
-			raise InvalidShapeError("Matrices must have the same shape")
+			raise InvalidShapeError
 
 	def _assert_square(self):
-		"""Asserts that the matrix is square.
-
-		Raises:
-			NotSquareError: If the matrix is not square.
-		"""
 		if not self.is_square():
 			raise NotSquareError
 
@@ -77,7 +38,6 @@ class Matrix:
 		return Matrix([[op(x, y) for x, y in zip(r1, r2)] for r1, r2 in zip(self.values, other.values)])
 
 	def _rref(self):
-		"""Transforms the matrix into reduced row echelon form."""
 		m = self.values
 		for i in range(self.rows()):
 			for j in range(self.cols()):
@@ -90,63 +50,23 @@ class Matrix:
 					break
 
 	def cols(self):
-		"""Returns the number of columns."""
 		return self.shape[1]
 
 	def rows(self):
-		"""Returns the number of rows."""
 		return self.shape[0]
 
 	def is_close(self, other: Matrix, delta=1e-9):
-		"""Returns whether the matrix is close to another matrix.
-
-		Args:
-			other (Matrix): The other matrix.
-			delta (float, optional): The maximum difference between the values.
-
-		Returns:
-			bool: Whether the matrix is close to another matrix.
-		"""
 		if self.shape != other.shape:
 			return False
 		return all(all(is_close(x, y, abs_tol=delta) for x, y in zip(r1, r2)) for r1, r2 in zip(self.values, other.values))
 
 	def is_square(self):
-		"""Returns whether the matrix is square or not."""
 		return self.cols() == self.rows()
 
-	def trace(self):
-		"""Computes the trace of the matrix.
-
-		Returns:
-			float: The trace of the matrix.
-
-		Raises:
-			NotSquareError: If the matrix is not square.
-		"""
-		self._assert_square()
-		return sum(self.values[i][i] for i in range(self.rows()))
-
 	def transpose(self):
-		"""Returns the transpose of the matrix."""
 		return Matrix([[self.values[j][i] for j in range(self.rows())] for i in range(self.cols())])
 
-	def row_echelon(self):
-		"""Returns the row echelon form of the matrix."""
-		m = deepcopy(self)
-		m._rref()
-		return m
-
 	def inverse(self):
-		"""Computes the inverse of the matrix.
-
-		Returns:
-			Matrix: The inverse of the matrix.
-
-		Raises:
-			NotSquareError: If the matrix is not square.
-			ValueError: If the matrix is not invertible.
-		"""
 		self._assert_square()
 		a2 = deepcopy(self)
 		dtype = type(self.values[0][0]) if self.cols() > 0 else float
@@ -163,54 +83,13 @@ class Matrix:
 			return a2
 		raise ValueError("Matrix is not invertible")
 
-	def rank(self):
-		"""Computes the rank of the matrix.
-
-		Returns:
-			int: The rank of the matrix.
-		"""
-		m = self.row_echelon()
-		return sum(any(x != 0 for x in r) for r in m.values)
-
 	def __add__(self, other):
 		if isinstance(other, Matrix):
 			return self._do_op_matrix(other, lambda x, y: x + y)
 		raise NotImplementedError
 
-	def __iadd__(self, other):
-		if isinstance(other, Matrix):
-			self._assert_same_shape(other)
-			for i in range(self.rows()):
-				for j in range(self.cols()):
-					self.values[i][j] += other.values[i][j]
-			return self
-		raise NotImplementedError
-
-	def __imul__(self, other):
-		if isinstance(other, (float, int)):
-			for i in range(self.rows()):
-				for j in range(self.cols()):
-					self.values[i][j] *= other
-			return self
-		if isinstance(other, Matrix):
-			self._assert_same_shape(other)
-			for i in range(self.rows()):
-				for j in range(self.cols()):
-					self.values[i][j] *= other.values[i][j]
-			return self
-		raise NotImplementedError
-
-	def __isub__(self, other):
-		if isinstance(other, Matrix):
-			self._assert_same_shape(other)
-			for i in range(self.rows()):
-				for j in range(self.cols()):
-					self.values[i][j] -= other.values[i][j]
-			return self
-		raise NotImplementedError
-
 	def __mul__(self, other):
-		if isinstance(other, (float, int)):
+		if isinstance(other, (Complex, float, int)):
 			return Matrix([[x * other for x in r] for r in self.values])
 		if isinstance(other, Matrix):
 			if self.cols() != other.rows():
@@ -228,9 +107,6 @@ class Matrix:
 		if isinstance(other, Matrix):
 			return self._do_op_matrix(other, lambda x, y: x - y)
 		raise NotImplementedError
-
-	def __repr__(self):
-		return f"Matrix({self.values})"
 
 	def __str__(self):
 		return str('\n'.join(str(r) for r in self.values))
